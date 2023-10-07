445ba0ce9bda4e2498098e8d367bfa7a73b1fbd3
Ujwal Dahal <109020653+Ujwallthehall@users.noreply.github.com>
2023-10-08 01:37:51 +0545

    third commit

--- script.js
+++
@@ -1,4 +0,0 @@
-// JavaScript code can be added here for interactivity if needed.
-document.getElementById('aboutButton').addEventListener('click', function () {
-    alert('About Us: Liquid Logic is a creative group dedicated to innovative solutions.');
-});
